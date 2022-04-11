d89b49d1d48054058cacd4805417904f875d6d99
Pritesh Ratnappagol <pritesh.ratnappagol@gmail.com>
2022-04-11 15:31:49 -0400

    Create class components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Button from './components/Button/Button';
+import HelloWorld from './components/Button/HelloWorld/HelloWorld';
 
 function App() {
   return (
     <div className='container mx-auto'>
-      <Button />  
+      <Button />
+      <HelloWorld />  
     </div>
   );
 }

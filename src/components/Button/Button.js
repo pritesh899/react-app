c340d5c0bf6149efd0d305554753e0ce831d9cdd
Pritesh Ratnappagol <pritesh.ratnappagol@gmail.com>
2022-04-11 15:29:29 -0400

    6. Create Function components

---
+++ src/components/Button/Button.js
@@ -0,0 +1,9 @@
+function Button() { 
+    return (
+        <div className='my-2'>
+            <button className='bg-red-400 hover:bg-red-900 text-white py-2 rounded-full'>Testing Testing</button>
+        </div>
+    )
+}
+
+export default Button;
